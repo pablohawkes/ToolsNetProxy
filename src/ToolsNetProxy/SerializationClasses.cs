022356effca648a5b42f0c9fb526f7afe530e8cd
Pablo <47606136+pablohawkes@users.noreply.github.com>
2020-09-10 20:16:07 -0300

    Only for backup. NOT operative yet
    Only for backup. NOT operative yet

---
+++ src/ToolsNetProxy/SerializationClasses.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace ToolsNetProxy
+{
+    public class Configuration
+    {
+        public Forwarders forwarders { get; set; }
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Configuration()
+        {
+
+            //Get configuration file location:
+            var directoryName = System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            var path = directoryName + @"\Configuration.xml";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Forwarders));
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                forwarders = (Forwarders)serializer.Deserialize(reader);
+                reader.Close();
+            }
+        }
+    }
+
+    [XmlRoot("Forwarders")]
+    public class Forwarders
+    {
+        [XmlElement("Forwarder")]
+        public List<Forwarder> Forwarder { get; set; }
+        public void StartActiveForwarders()
+        {
+            foreach (var forw in Forwarder)
+            {
+                if (forw.Active)
+
+                    forw.StartForwarder();
+            }
+        }
+    }
+
+    [XmlRoot("Forwarder")]
+    public class Forwarder
+    {
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("Active")]
+        public bool Active { get; set; }
+
+        [XmlAttribute("SourceIp")]
+        public string SourceIp { get; set; }
+
+        [XmlAttribute("SourcePort")]
+        public int SourcePort { get; set; }
+
+        [XmlAttribute("DestinationIp")]
+        public string DestinationIp { get; set; }
+
+        [XmlAttribute("DestinationPort")]
+        public int DestinationPort { get; set; }
+
+        [XmlElement("ReplaceString")]
+        public List<ReplaceString> replaceString { get; set; }
+
+        Thread t;
+        private Socket _mainSocket;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void StartForwarder()
+        {
+            t = new Thread(new ThreadStart(ThreadWorker));
+            t.Start();
+        }
+
+        private void ThreadWorker()
+        {
+            /* TODO
+            var LocalEndPoint = new IPEndPoint(IPAddress.Parse("0.0.0.0"))
+            _mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); ;
+
+            _mainSocket.Bind(local);
+            _mainSocket.Listen(10);
+
+            while (true)
+            {
+                var source = _mainSocket.Accept();
+                var destination = new TcpForwarderSlim();
+                var state = new State(source, destination._mainSocket);
+                destination.Connect(remote, source);
+                source.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+            }
+            */
+        }
+    }
+
+    [XmlRoot("ReplaceString")]
+    public class ReplaceString
+    {
+        [XmlAttribute("OriginalString")]
+        public string OriginalString { get; set; }
+        [XmlAttribute("NewString")]
+        public string NewString { get; set; }
+    }
+}
---
+++ src/ToolsNetProxy/ToolsNetProxy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsNetProxy
+{
+    public partial class ToolsNetProxy : ServiceBase
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        Configuration config;
+
+        public ToolsNetProxy()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //UNCOMMENT TO ENABLE DEBUGGER:
+            //System.Diagnostics.Debugger.Launch();
+
+            log.Info("***********************************************************************************************");
+            log.Info("OnStart event - Version: " + Assembly.GetExecutingAssembly().GetName().Version);
+
+            //Load Forwarding configuration:
+            try
+            {
+                config = new Configuration();
+            }
+            catch (Exception exc)
+            {
+                log.Error("Error on Load Configuration MXL file", exc);
+
+                return;
+            }
+
+            //TODO
+            foreach (var a in config.forwarders.Forwarder)
+            {
+                log.Debug(a.Name);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("OnStop event");
+            log.Info("***********************************************************************************************");
+        }
+
+        protected override void OnShutdown()
+        {
+            log.Info("OnShutdown event");
+            log.Info("***********************************************************************************************");
+        }
+
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            log.Info("OnPowerEvent event - cause: " + powerStatus.ToString());
+
+            return base.OnPowerEvent(powerStatus);
+        }
+    }
+}
